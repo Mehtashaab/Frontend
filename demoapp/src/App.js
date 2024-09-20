0efdc066a7ac50fd669204b8222e8eac8e9d6448
Mehtashaab <mehtaabhishek499@gmail.com>
2024-09-20 11:50:36 +0530

    updated the code base

---
+++ demoapp/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Lets start react || Abhishek Mehta</h1>
+  );
+}
+
+export default App;
---
+++ react-viteApp/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>Lets start with react + vite | Abhishek Mehta</h1>
+      
+  )
+}
+
+export default App
