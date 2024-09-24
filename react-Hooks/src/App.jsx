9a88c8979ba7c80c1e62e8e7c21b592564fb9d5a
Mehtashaab <mehtaabhishek499@gmail.com>
2024-09-24 13:09:17 +0530

    Use the Hooks of react use useState hooks to change the UI of the project Apply the validation to the UI

---
+++ react-Hooks/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count,setCount]=useState(15)
+
+// use Hooks to change the state 
+
+  // const addValue = ()=>{
+  //   count = count + 1
+  //   setCount(count)
+  // }
+  // const removeValue = ()=>{
+  //   count = count - 1
+  //   setCount(count)
+  // }
+
+
+//apply validation to stop the addvalue to 20 and stop the remove value to 0
+
+  const addValue = ()=>{
+    if(count < 20){
+      setCount(count+1)
+    }
+  }
+  const removeValue = ()=>{
+    if(count > 0){
+      setCount(count-1)
+    }
+  }
+  
+// let count = 15
+// const addValue = ()=>{
+//   count = count + 1 
+//   console.log(count)
+// }
+// const removeValue = ()=>{
+//   count = count - 1
+//   console.log(count)
+// }
+  return (
+    <>
+     <h1>Hello World!</h1>
+      <h2>Count : {count}</h2>
+     <button onClick={addValue}>addValue :{count}</button>
+     <button onClick={removeValue}>removeValue :{count}</button>
+    </>
+  )
+}
+
+export default App
